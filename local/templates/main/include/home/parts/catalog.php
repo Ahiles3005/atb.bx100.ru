ea891350d9d16321b6c17e4e207357386677203d
Одногулов Руслан Хосянович <orh@cds.guru>
2025-11-06 15:15:56 +0300

    вывод списка каталогов на главной странице, без вывод элементов каталога

--- local/templates/main/include/home/parts/catalog.php
+++ local/templates/main/include/home/parts/catalog.php
@@ -1,3 +1,43 @@
+
+
+<?
+$APPLICATION->IncludeComponent(
+        "bitrix:catalog.section.list",
+        "section_list_home",
+        array(
+                "ADDITIONAL_COUNT_ELEMENTS_FILTER" => "additionalCountFilter",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "CACHE_FILTER" => "N",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "COUNT_ELEMENTS" => "Y",
+                "COUNT_ELEMENTS_FILTER" => "CNT_ACTIVE",
+                "FILTER_NAME" => "sectionsFilter",
+                "HIDE_SECTIONS_WITH_ZERO_COUNT_ELEMENTS" => "N",
+                "IBLOCK_ID" => "1",
+                "IBLOCK_TYPE" => "catalog",
+                "SECTION_CODE" => "",
+                "SECTION_FIELDS" => array(
+                        0 => "",
+                        1 => "",
+                ),
+                "SECTION_ID" => "",
+                "SECTION_URL" => "",
+                "SECTION_USER_FIELDS" => array(
+                        0 => "UF_NAME_MENU_HOME",
+                        1 => "",
+                ),
+                "SHOW_PARENT_NAME" => "Y",
+                "TOP_DEPTH" => "2",
+                "VIEW_MODE" => "LINE",
+                "COMPONENT_TEMPLATE" => "section_list_home"
+        ),
+        false
+);
+?>
+
+
 <section class="hm-cat">
     <div class="hm-cat--div__CONT C-CONTAINER">
         <div class="c-common--div__HEAD">
