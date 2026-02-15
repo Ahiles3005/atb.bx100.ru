5c15c259b0bf4ec253297a4c7eb8a734c2a7ce01
Одногулов Руслан Хосянович <orh@cds.guru>
2026-02-15 14:57:06 +0300

    изменения названия и кода свойства

--- local/templates/main/components/bitrix/catalog.section/industries_series/template.php
+++ local/templates/main/components/bitrix/catalog.section/industries_series/template.php
@@ -46,7 +46,7 @@ if (!empty($arResult['ITEMS'])) {
                 <a class="hm-ind--a__CARD_TAG color-yellow" href="<?= $item['DETAIL_PAGE_URL'] ?>">
                     <div class="hm-ind--div__CARD_TAG_CIRCLE"></div>
                     <span class="hm-ind--span__CARD_TAG">
-                            <?= $item['DISPLAY_PROPERTIES']['TAG_KRASOTA']['VALUE'] ?? '' ?>
+                            <?= $item['DISPLAY_PROPERTIES']['OTRASLI']['VALUE'] ?? '' ?>
                         </span>
                 </a>
                 <p class="hm-ind--p__CARD_NAME">
