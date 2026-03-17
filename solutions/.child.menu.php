45e838ddac54ce1e0e8fd530be5f0c59ef77875f
Одногулов Руслан Хосянович <orh@cds.guru>
2026-03-17 23:04:01 +0300

    меню для решений и индустрии

--- solutions/.child.menu.php
+++ solutions/.child.menu.php
@@ -1,3 +1,18 @@
 <?php
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 
+global $APPLICATION;
+$aMenuLinksExt = $APPLICATION->IncludeComponent("bitrix:menu.sections", "", [
+    "IS_SEF" => "Y",
+    "SEF_BASE_URL" => "/sections/",
+    "SECTION_PAGE_URL" => "#SECTION_CODE_PATH#/",
+    "DETAIL_PAGE_URL" => "#SECTION_CODE_PATH#/#CODE#",
+    "IBLOCK_TYPE" => "content",
+    "IBLOCK_ID" => "3",
+    "DEPTH_LEVEL" => "3",
+    "CACHE_TYPE" => "A",
+    "CACHE_TIME" => "3600"
+], false
+);
+
+$aMenuLinks = array_merge($aMenuLinks, $aMenuLinksExt);
