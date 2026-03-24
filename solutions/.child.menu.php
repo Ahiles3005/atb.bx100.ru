1c18a18d11cae875cbf6e82b6b935f30c6c7e74f
Одногулов Руслан Хосянович <orh@cds.guru>
2026-03-24 12:46:35 +0300

    fix menu solutions

--- solutions/.child.menu.php
+++ solutions/.child.menu.php
@@ -4,7 +4,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
 global $APPLICATION;
 $aMenuLinksExt = $APPLICATION->IncludeComponent("bitrix:menu.sections", "", [
     "IS_SEF" => "Y",
-    "SEF_BASE_URL" => "/sections/",
+    "SEF_BASE_URL" => "/solutions/",
     "SECTION_PAGE_URL" => "#SECTION_CODE_PATH#/",
     "DETAIL_PAGE_URL" => "#SECTION_CODE_PATH#/#CODE#",
     "IBLOCK_TYPE" => "content",
