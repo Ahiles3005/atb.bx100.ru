19fcf0f9bf8f6a7ae44923417577df678cea7052
Одногулов Руслан Хосянович <orh@cds.guru>
2026-04-30 11:13:58 +0300

    начало внедрения страницы о компании

--- about/index.php
+++ about/index.php
@@ -1,9 +1,73 @@
-<?php
+<?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("О КОМПАНИИ");
-?>
-<div class="C-CONTAINER">
-    <!-- Страница раздела: О КОМПАНИИ -->
-</div>
-<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetPageProperty('mainid', 'ab');
 
+?>
+
+
+<?require_once (__DIR__.'/content.php')?>
+
+
+<?$APPLICATION->IncludeComponent(
+        "bitrix:news.detail",
+        "about_index",
+        Array(
+                "ACTIVE_DATE_FORMAT" => "",
+                "ADD_ELEMENT_CHAIN" => "N",
+                "ADD_SECTIONS_CHAIN" => "N",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "N",
+                "BROWSER_TITLE" => "-",
+                "CACHE_GROUPS" => "N",
+                "CACHE_TIME" => "3600",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "N",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "N",
+                "DISPLAY_DATE" => "N",
+                "DISPLAY_NAME" => "N",
+                "DISPLAY_PICTURE" => "N",
+                "DISPLAY_PREVIEW_TEXT" => "N",
+                "DISPLAY_TOP_PAGER" => "N",
+                "ELEMENT_CODE" => "",
+                "ELEMENT_ID" => "95",
+                "FIELD_CODE" => array("",""),
+                "FILE_404" => '',
+                "GROUP_PERMISSIONS" => '',
+                "IBLOCK_ID" => "10",
+                "IBLOCK_TYPE" => "about_index",
+                "IBLOCK_URL" => "",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                "MESSAGE_404" => "",
+                "META_DESCRIPTION" => "-",
+                "META_KEYWORDS" => "-",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_SHOW_ALWAYS" => "N",
+                "PAGER_TEMPLATE" => "",
+                "PAGER_TITLE" => "",
+                "PROPERTY_CODE" => array("NASHI_CENNOSTY","KOORP_POLITIKA_1","KOORP_POLITIKA_2","SLOVO_KOMANDY","PREIMUSHESTVA","FAQ","ISTORIY_ROSTA",""),
+                "SET_BROWSER_TITLE" => "N",
+                "SET_CANONICAL_URL" => "N",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "N",
+                "SET_META_KEYWORDS" => "N",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "N",
+                "SHOW_404" => "N",
+                "STRICT_SECTION_CHECK" => "N",
+                "USE_PERMISSIONS" => "N",
+                "USE_SHARE" => "N"
+        )
+);?>
+
+
+
+
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
