e54640a8bf5baf4b76c609b0f117e6863b5e4e95
Одногулов Руслан Хосянович <orh@cds.guru>
2026-05-10 01:59:57 +0300

    исправил шаблон + создал снипет

--- bitrix/templates/.default/snippets/.content.php
+++ bitrix/templates/.default/snippets/.content.php
@@ -1,5 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $SNIPPETS = Array();
-$SNIPPETS['snippet0001.snp'] = Array('title' => 'Описание по умолчанию');
+$SNIPPETS['snippet0001.snp'] = Array('title' => 'Описание для раздела (шаблон)');
+$SNIPPETS['snippet0002.snp'] = Array('title' => 'Описание для товара (шаблон)');
 ?>
