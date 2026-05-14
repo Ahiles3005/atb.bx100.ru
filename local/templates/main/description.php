9aa7c295814a99ffba4b53040a842df5ba7202e7
Одногулов Руслан Хосянович <orh@cds.guru>
2026-05-15 02:56:28 +0300

    подключение стилей для визуального редактора

--- local/templates/main/description.php
+++ local/templates/main/description.php
@@ -7,6 +7,9 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 $arTemplate = [
     'NAME' => 'main',
     'DESCRIPTION' => 'Основной шаблон сайта',
+    'EDITOR_STYLES' => [
+        '/local/templates/main/assets/assets/pages/catalog/css/catalog3-abt.css'
+    ],
 ];
 ?>
 
