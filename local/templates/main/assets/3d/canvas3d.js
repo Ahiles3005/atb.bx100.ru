8d8ae39941f7250f7998dbc97c54f0c93de89d7b
Одногулов Руслан Хосянович <orh@cds.guru>
2026-07-25 18:56:56 +0300

    исправил отображение 3д обьектов елси их больше 1 в галереи

--- local/templates/main/assets/3d/canvas3d.js
+++ local/templates/main/assets/3d/canvas3d.js
@@ -10,81 +10,87 @@ import { DRACOLoader } from "dracoloader";
 
 
 
+
+const canvases = document.querySelectorAll(".cd-hero--canvas");
+
+canvases.forEach((canvas) => {
+
 // 1. СОЗДАНИЕ РЕНДЕРА, СЦЕНЫ И КАМЕРЫ
 
-const canvas = document.querySelector(".cd-hero--canvas");
-const renderer = new THREE.WebGLRenderer({antialias: true, canvas, alpha: true});
-const scene = new THREE.Scene ();
-const camera = new THREE.PerspectiveCamera (5, window.innerWidth / window.innerHeight, 0.1, 1000);
+  //  const canvas = document.querySelector(".cd-hero--canvas");
+    const renderer = new THREE.WebGLRenderer({antialias: true, canvas, alpha: true});
+    const scene = new THREE.Scene ();
+    const camera = new THREE.PerspectiveCamera (5, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
 
 
 // 2. СОХРАНЕНИЕ ЧЕТКОСТИ ИЗОБРАЖЕНИЯ В Т.Ч. ПРИ РЕСАЙЗЕ
 
-function resizeRendererToDisplaySize(renderer) {
-    const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
-    const needResize = canvas.width !== width || canvas.height !== height;
-    if (needResize) {
-      renderer.setSize(width, height, false);
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
     }
-    return needResize;
-}
 
-resizeRendererToDisplaySize(renderer);
-
-camera.aspect = canvas.clientWidth / canvas.clientHeight;
-camera.updateProjectionMatrix();
-
-
-function cdHero3dDebounce (cB, time) {
-    let idTimer;
-    return function () {
-        clearTimeout (idTimer);
-        idTimer = setTimeout (() => {
-            cB();
-        }, time);
-    }
-}
-
-const cdHero3dDebounce1 = cdHero3dDebounce (() => {
     resizeRendererToDisplaySize(renderer);
 
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
-}, 100);
 
-window.addEventListener ("resize", cdHero3dDebounce1);
+
+    function cdHero3dDebounce (cB, time) {
+        let idTimer;
+        return function () {
+            clearTimeout (idTimer);
+            idTimer = setTimeout (() => {
+                cB();
+            }, time);
+        }
+    }
+
+    const cdHero3dDebounce1 = cdHero3dDebounce (() => {
+        resizeRendererToDisplaySize(renderer);
+
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+    }, 100);
+
+    window.addEventListener ("resize", cdHero3dDebounce1);
 
 
 
 
 // 3. ЗАГРУЗЧИК МОДЕЛЕЙ ФОРМАТА GLTF (GLB) И ЗАГРУЗКА МОДЕЛИ
 
-const loader = new GLTFLoader ();
-const drloader = new DRACOLoader ();
-drloader.setDecoderPath ("3d/dracoFiles/");
-loader.setDRACOLoader (drloader);
-{
-    const d3 = canvas.dataset.d;
-    loader.load (`${d3}`, function (gltf) {
-        scene.add(gltf.scene);
-    });
-}
+    const loader = new GLTFLoader ();
+    const drloader = new DRACOLoader ();
+    drloader.setDecoderPath ("/local/templates/main/assets/3d/dracoFiles/");
+
+    loader.setDRACOLoader (drloader);
+    {
+        const d3 = canvas.dataset.d;
+        loader.load (`${d3}`, function (gltf) {
+            scene.add(gltf.scene);
+        });
+    }
 
 
 
 
 // 4. ОСВЕЩЕНИЕ ДВУХ ВИДОВ
 
-const ambientLight = new THREE.AmbientLight(0x404040, 10.6);
+    const ambientLight = new THREE.AmbientLight(0x404040, 10.6);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-directionalLight.position.set(5, 5, 5);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(5, 5, 5);
 
-scene.add(ambientLight, directionalLight);
+    scene.add(ambientLight, directionalLight);
 
 
 
@@ -99,20 +105,21 @@ scene.add(ambientLight, directionalLight);
 
 // 6. УПРАВЛЕНИЕ С ПОМОЩЬЮ МЫШИ
 
-const controls = new OrbitControls(camera, canvas);
-camera.position.z = 5;
+    const controls = new OrbitControls(camera, canvas);
+    camera.position.z = 5;
 
 
 
 
 // 7. РЕНДЕРИНГ С УСТАНОВЛЕННОЙ ВОЗМОЖНОСТЬЮ АНИМАЦИИ
 
-function animate() {
-    requestAnimationFrame(animate)
-    
-    controls.update();
-    
-    renderer.render (scene, camera);
-}
+    function animate() {
+        requestAnimationFrame(animate)
 
-animate ();
+        controls.update();
+
+        renderer.render (scene, camera);
+    }
+
+    animate ();
+});
