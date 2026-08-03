0fdf1661e52683f0d8a7852e56baf4f315a73651
Одногулов Руслан Хосянович <orh@cds.guru>
2026-08-03 10:14:22 +0300

    стили для редактора

--- local/templates/main/description.php
+++ local/templates/main/description.php
@@ -8,9 +8,21 @@ $arTemplate = [
     'NAME' => 'main',
     'DESCRIPTION' => 'Основной шаблон сайта',
     'EDITOR_STYLES' => [
-        '/local/templates/main/assets/assets/pages/catalog/css/catalog3-abt.css'
+        '/local/templates/main/assets/assets/pages/catalog/css/catalog3-abt.css',
+
+        //ne
+        '/local/templates/main/assets/assets/components/share/comp-share.css',
+        '/local/templates/main/assets/assets/pages/home/css/home2-cat.css',
+        '/local/templates/main/assets/assets/pages/home/css/home3-ind.css',
+        '/local/templates/main/assets/assets/pages/home/css/home4-des.css',
+        '/local/templates/main/assets/assets/pages/card/css/card7-use.css',
+        '/local/templates/main/assets/assets/pages/card-n/css/card-n4-reg.css',
+        '/local/templates/main/assets/assets/pages/card-n/css/card-n5-rec.css',
+        '/local/templates/main/assets/assets/pages/ind/css/in4-des.css',
+        '/local/templates/main/assets/assets/pages/mc/css/mc6-pk.css',
+        '/local/templates/main/assets/assets/pages/ane/css/ane0-common.css',
+        '/local/templates/main/assets/assets/pages/ane/css/ane1-hero.css',
+        '/local/templates/main/assets/assets/pages/ane/css/ane2-main.css',
     ],
 ];
 ?>
-
-
